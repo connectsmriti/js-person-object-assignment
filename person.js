0e6580f792e9019069efed82ae8cbb1239c23036
connectsmriti <90024751+connectsmriti@users.noreply.github.com>
2021-11-26 21:44:17 +0530

    Add files via upload

---
+++ person.js
@@ -0,0 +1,21 @@
+var person1 = {
+  FirstName: "Smriti",
+  LastName: "Gupta",
+  OfficeAdress: "Near Gandhi chowk, Malviya Nagar",
+};
+
+var person2 = {
+  FirstName: "Isha",
+  LastName: "Gupta",
+  OfficeAdress: "Near Gandhi Maidan, Patna, Bihar",
+};
+
+var person3 = {
+  FirstName: "Neha",
+  LastName: "Sharma",
+  OfficeAdress: "Vikas Nagar, Road 7, Kolkata",
+};
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
